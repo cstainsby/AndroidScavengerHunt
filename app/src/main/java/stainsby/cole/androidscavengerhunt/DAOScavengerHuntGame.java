1970c0f9d2b64200f246d74ff07814234983a18d
cstainsby <cstainsby@zagmail.gonzaga.edu>
2021-12-07 15:19:50 -0800

    added sha-1 to firebase build

--- app/src/main/java/stainsby/cole/androidscavengerhunt/DAOScavengerHuntGame.java
+++ app/src/main/java/stainsby/cole/androidscavengerhunt/DAOScavengerHuntGame.java
@@ -3,6 +3,8 @@ package stainsby.cole.androidscavengerhunt;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.List;
+
 public class DAOScavengerHuntGame {
     private DatabaseReference mDatabase;
 
@@ -20,6 +22,7 @@ public class DAOScavengerHuntGame {
         ScavengerHuntGame game = new ScavengerHuntGame(title);
         mDatabase.child("game").child(title).setValue(game);
     }
+
 
     public void deleteUser(String userId) {
         mDatabase.child("game").child(userId).removeValue();
