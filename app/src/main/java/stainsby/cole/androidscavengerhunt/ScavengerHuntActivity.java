ba33832c18c3b00423f93e6ad05d1620b3968d84
cstainsby <cstainsby@zagmail.gonzaga.edu>
2021-12-15 15:05:19 -0800

    final changes from cole branch

--- app/src/main/java/stainsby/cole/androidscavengerhunt/ScavengerHuntActivity.java
+++ app/src/main/java/stainsby/cole/androidscavengerhunt/ScavengerHuntActivity.java
@@ -127,18 +127,14 @@ public class ScavengerHuntActivity extends FragmentActivity implements OnMapRead
         Log.d(TAG, "loadGameAttributes: drawing circle on user position with radius " + radius);
         drawCircle(meanCoordinates, 10000.0f);
         addGeofence(meanCoordinates, 10000.0f);
+
+        for(LatLng latLng : scavengerLocations) {
+            drawCircle(latLng, 1000.0f);
+            addGeofence(latLng, 1000.0f);
+        }
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
